c12381c65ed52edc4f2e506a2b1cca42a6f93541
siarhei kharlap <siarhei.kharlap@ventionteams.com>
2024-11-14 09:35:36 +0100

    Add HttpClient BaseUri experiment

---
+++ experiments/HttpClientBaseUri/ListnerApp/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    var logger = context.RequestServices.GetService<ILogger<Program>>();
+
+    logger.LogInformation(context.Request.Path);
+
+    await next(context);
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+partial class Program();
---
+++ experiments/HttpClientBaseUri/Tester/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddHttpClient(
+    "test_without",
+    (configure) =>
+    {
+        configure.BaseAddress = new Uri("http://localhost:5000/prefix_without");
+    });
+
+builder.Services.AddHttpClient(
+    "test_with",
+    (configure) =>
+    {
+        configure.BaseAddress = new Uri("http://localhost:5000/prefix_with/");
+    });
+
+var app = builder.Build();
+
+
+var factory = app.Services.GetService<IHttpClientFactory>() ?? throw new NullReferenceException();
+
+var clientWithout = factory.CreateClient("test_without");
+var clientWith = factory.CreateClient("test_with");
+
+await clientWithout.GetAsync("/after_with"); // http://localhost:5000/after_with 
+await clientWithout.GetAsync("after_without"); // http://localhost:5000/after_without
+
+await clientWith.GetAsync("/after_with"); // http://localhost:5000/after_with
+await clientWith.GetAsync("after_without"); // http://localhost:5000/prefix_with/after_without
+
+await Task.Delay(10000);
