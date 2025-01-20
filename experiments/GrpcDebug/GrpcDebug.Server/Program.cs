0a2e17e9cdd008ccdd67eef1d25a650dce8ae6dd
siarhei kharlap <siarhei.kharlap@gmail.com>
2025-01-20 17:41:39 +0100

    Remove all limits

--- experiments/GrpcDebug/GrpcDebug.Server/Program.cs
+++ experiments/GrpcDebug/GrpcDebug.Server/Program.cs
@@ -2,28 +2,9 @@ using GrpcDebug.Server.Services;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.Limits.MaxConcurrentConnections = 10;
-    serverOptions.Limits.MinRequestBodyDataRate = 
-        new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
-    serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(20);
-    serverOptions.Limits.Http2.MaxStreamsPerConnection = 10;
-    serverOptions.Limits.Http2.MaxFrameSize = 16_384;
-    serverOptions.Limits.Http2.MaxRequestHeaderFieldSize = 8192;
-    serverOptions.Limits.Http2.InitialStreamWindowSize = 65535;
-    serverOptions.Limits.Http2.InitialConnectionWindowSize = 65535;
-    serverOptions.Limits.Http2.KeepAlivePingDelay = TimeSpan.FromSeconds(10);
-    serverOptions.Limits.Http2.KeepAlivePingTimeout = TimeSpan.FromSeconds(20);
-});
 
 // Add services to the container.
-builder.Services.AddGrpc(
-    options =>
-    {
-        options.MaxSendMessageSize = 2 * 1024 * 1024 / 10;
-        
-    });
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
